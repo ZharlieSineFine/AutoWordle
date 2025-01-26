b2823c3ee78ecac5044fa44ae16e78308d6cae71
Zhao Changlin <huskydawg@foxmail.com>
2025-01-26 12:29:12 +0800

    Declare the CONSTANT and the global variables

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ from words import WORDS
 
 # A pretty good start can be "soare".
 DEFAULT_GUESS = "soare"
+
+# DEFAULT_GUESS = random.choice(WORDS)
+
+new_guess = list(DEFAULT_GUESS)
+
+# Track letters that are present but not in the right spot
+present_letters = {}
+
+# Track letters that should not be included in future guesses
+absent_letters = set()
+
+all_correct = False
+
